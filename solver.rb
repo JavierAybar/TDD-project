325fb3124d14e9036fa5e7892a943c724373f0d8
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-10-13 15:06:14 -0300

    Add methods factorial and reverse

--- solver.rb
+++ solver.rb
@@ -1,2 +1,15 @@
 class Solver
+  def factorial(num)
+    raise ArgumentError, 'Argument should be an positive integer' if num.negative?
+
+    if num.zero?
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
+
+  def reverse(word)
+    word.reverse
+  end
 end
